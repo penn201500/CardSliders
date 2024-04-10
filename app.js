8fac6f935e033537cd108d9301c5ae2340e52a34
penn <penn201500@gmail.com>
2024-04-11 00:34:21 +0800

    implement function to display the card that is clicked

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import elements from "./sliders";
+import elements from "./sliders.js";
 
 const { slider, sliders } = elements;
 
@@ -13,3 +13,28 @@ const leftMove = () => {
 };
 
 
+sliders.forEach((slide) => {
+    slide.addEventListener("click", () => {
+      const slideIndex = [...slider.children].indexOf(slide);
+      console.log(`Here is the slide index: ${slideIndex}`);
+
+      switch (slideIndex) {
+        case 0:
+          leftMove();
+          leftMove();
+          break;
+        case 1:
+          leftMove();
+          break;
+        case 3:
+          rightMove();
+          break;
+        case 4:
+          rightMove();
+          rightMove();
+          break;
+        default:
+          break;
+      }
+    }
+)});
